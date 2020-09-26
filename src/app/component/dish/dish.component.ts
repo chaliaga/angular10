6552703476f173e6597cd55cb5e5f465946b0b18
i010481 <christian.aliaga@sap.com>
2020-09-26 13:18:47 -0500

    First Import

---
+++ src/app/component/dish/dish.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Ingredient } from '../../interface/ingredient';
+
+@Component({
+  selector: 'app-dish',
+  templateUrl: './dish.component.html',
+  styleUrls: ['./dish.component.scss']
+})
+export class DishComponent implements OnInit {
+  // tslint:disable-next-line:variable-name
+  private _name;
+  get name(): string {
+    return this._name;
+  }
+  @Input() set name(value: string) {
+    this._name = value;
+  }
+
+  // tslint:disable-next-line:variable-name
+  private _ingredients;
+  get ingredients(): Ingredient[]{
+    return this._ingredients;
+  }
+  @Input() set ingredients(value: Ingredient[]) {
+    this._ingredients = value;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
